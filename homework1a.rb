f81d8a87c1f611f9e0f7fa8d56fa741fa0625230
nepttako <nepttako@gmail.com>
2016-05-05 16:40:18 +0800

    add loop and end

--- homework1a.rb
+++ homework1a.rb
@@ -10,47 +10,46 @@ begin #可以美化輸出
   puts "|===============================================|" 
   puts "|Welcome to Rock Paper Scissors!!!              |"
   puts "|===============================================|"
+end
+
+loop do
 
   begin 
     puts "please choose one of the following: R / P / S"
     user_input = gets.chomp.upcase
   end while !["R", "P", "S"].include?(user_input) 
-
-  begin
-    puts "You choose #{user_input}"
-
+  puts "You choose #{user_input}"
   #接下來請把剩下的部份寫出來...
 
 
-arr = ["R","P","S"]
-computer_input = arr.sample
+  arr = ["R","P","S"]
+  computer_input = arr.sample
 
-begin 
   puts "Computer choose #{computer_input}"
   puts "So who win?"
 
-def Rock_Paper_Scissors_rules(user_input,computer_input)
-  if user_input == computer_input
-    puts "You two are Tie!"
-  elsif (user_input == "R") && (computer_input == "P")
-    puts "Paper beats Rock, You Lose!"
-  elsif (user_input == "R") && (computer_input == "S")
-    puts "Rock beats Scissors, You Win!"
-  elsif (user_input == "P") && (computer_input == "R")
-    puts "Paper beats Rock, You Win!"
-  elsif (user_input == "P") && (computer_input == "S")
-    puts "Scissors beats Paper, You Lose!"
-  elsif (user_input == "S") && (computer_input == "R")
-    puts "Rock beats Scissors, You Lose!"
-  elsif (user_input == "S") && (computer_input == "P")
+  def Rock_Paper_Scissors_rules(user_input,computer_input)
+    if user_input == computer_input
+      puts "You two are Tie!"
+    elsif (user_input == "R") && (computer_input == "P")
+      puts "Paper beats Rock, You Lose!"
+    elsif (user_input == "R") && (computer_input == "S")
+      puts "Rock beats Scissors, You Win!"
+    elsif (user_input == "P") && (computer_input == "R")
+      puts "Paper beats Rock, You Win!"
+    elsif (user_input == "P") && (computer_input == "S")
+      puts "Scissors beats Paper, You Lose!"
+    elsif (user_input == "S") && (computer_input == "R")
+      puts "Rock beats Scissors, You Lose!"
+    elsif (user_input == "S") && (computer_input == "P")
     puts "Scissors beats Paper, You Win!"
-  else
-    puts "Call Maintenance!"
-  end   
-end
+    else
+      puts "Call Maintenance!"
+    end   
+  end
 
-result = Rock_Paper_Scissors_rules(user_input,computer_input)
-puts result 
+  result = Rock_Paper_Scissors_rules(user_input,computer_input)
+  puts result 
 
   #問使用者是否還要再玩 
   begin
@@ -58,7 +57,10 @@ puts result
     continue = gets.chomp.upcase
   end while !["Y", "N"].include?(continue)
 
-end while continue == "N"
-# 若使用者回答 "N"，印出離開的訊息，不是就回到迴圈的上層繼續玩
-puts "Good Bye! Thanks for playing!"
+  if continue == "N"
+    puts "GoodBye! Thanks for playing!"
+    break
+  # 若使用者回答 "N"，印出離開的訊息，不是就回到迴圈的上層繼續玩
+    
+  end
 end
